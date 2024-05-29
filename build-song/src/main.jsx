3c1d2905c2203bbf6bbed0e1cb2c58f3ab744f21
Kevin Cueva <kevinneillcuevaalmendrales@gmail.com>
2024-05-29 15:02:18 -0500

    Coments

--- build-song/src/main.jsx
+++ build-song/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Landing from './View/Landing/Landing'
-
+//Componente Principal
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Landing />
